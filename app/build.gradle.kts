f0529223111aa91ec6405ef41d2d00759194a378
Manny Garcia <mannyalgarcia@gmail.com>
2025-12-06 19:50:36 -0800

    installed material2 and finished landing page xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.google.android.material:material:<version>")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
