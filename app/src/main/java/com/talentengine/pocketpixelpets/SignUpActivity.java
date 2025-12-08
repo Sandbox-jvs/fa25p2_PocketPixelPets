1be6b65aa0d414685293a5fe5b6af436ac68903a
Manny Garcia <mannyalgarcia@gmail.com>
2025-12-07 19:44:25 -0800

    XML for ChoosePetActivity

--- app/src/main/java/com/talentengine/pocketpixelpets/SignUpActivity.java
+++ app/src/main/java/com/talentengine/pocketpixelpets/SignUpActivity.java
@@ -14,7 +14,7 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
 
         otterSpriteView = findViewById(R.id.otterSpriteView);
         otterSpriteView.setSpriteSheet(
