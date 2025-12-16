829270b7a31b426a93907355bb68601e67c3905b
Jessica Sandoval <1991jessica.sandoval@gmail.com>
2025-12-15 23:46:10 -0800

    Reference to toy xml buttons set; toast for ball if selected.

--- app/src/main/java/com/talentengine/pocketpixelpets/ChooseToyActivity.java
+++ app/src/main/java/com/talentengine/pocketpixelpets/ChooseToyActivity.java
@@ -2,7 +2,9 @@ package com.talentengine.pocketpixelpets;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +14,34 @@ public class ChooseToyActivity extends AppCompatActivity {
     private String username;
     private Button nextButton;
 
+    private View ballButton;
+    private View plushieButton;
+    private View stickButton;
+    private View bubbleWandButton;
+    private String selectedToy = null;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_toy);
 
         username = getIntent().getStringExtra("USERNAME");
+        // Reference to the card views inside toy xml
+        ballButton = findViewById(R.id .ballButton);
+        plushieButton = findViewById(R.id .plushieButton);
+        stickButton = findViewById(R.id .stickButton);
+        bubbleWandButton = findViewById(R.id .bubbleWandButton);
+
+        // Wire with intents
+        ballButton.setOnClickListener(v -> {
+            selectedToy = "Ball";
+            Toast.makeText(this, "Boing! Boing!", Toast.LENGTH_SHORT).show();
+                });
 
         nextButton = findViewById(R.id.nextButtonToy);
         nextButton.setOnClickListener(v -> goToNextStep());
+
     }
 
     private void goToNextStep() {
