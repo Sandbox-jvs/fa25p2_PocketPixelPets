dad7f57c1d480c54f0bc2e66c9e99cc5e16e99e2
Jessica Sandoval <1991jessica.sandoval@gmail.com>
2025-12-15 19:17:03 -0800

    Minor changes; cosmetic

--- app/src/main/java/com/talentengine/pocketpixelpets/ChoosePersonalityActivity.java
+++ app/src/main/java/com/talentengine/pocketpixelpets/ChoosePersonalityActivity.java
@@ -1,33 +1,66 @@
 package com.talentengine.pocketpixelpets;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * @author Jessica Sandoval
+ * <br>
+ * Personality Activity requires, at minimum, onCreate(), to extend AppCompatActivity,
+ * and a call to setContentView()
+ */
 public class ChoosePersonalityActivity extends AppCompatActivity {
-
-    private String username;
-    private Button nextButton;
+    //Will be updated when a user selects Cheerful, Lazy, Playful, or Clean Freak
+    private String selectedPersonality = null;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_personality);
 
-        username = getIntent().getStringExtra("USERNAME");
+        //Get references to the views within the xml file
+        Button btnNext = findViewById(R.id.nextButton);
+        View btnCheerful = findViewById(R.id.cheerfulButton);
+        View btnLazy = findViewById(R.id.lazyButton);
+        View btnPlayful = findViewById(R.id.playfulButton);
+        View btnCleanFreak = findViewById(R.id.cleanFreakButton);
 
-        nextButton = findViewById(R.id.nextButtonPersonality);
-        nextButton.setOnClickListener(v -> goToNextStep());
+
+
+        //Wire up with intents
+        btnNext.setOnClickListener(v -> {
+            if (selectedPersonality == null) {
+                Toast.makeText(this, "Oops! Please select a personality for your pet!", Toast.LENGTH_SHORT).show();
+            }
+
+            Intent intent = new Intent(this, ChooseFoodActivity.class);
+            startActivity(intent);
+        });
+
+        btnLazy.setOnClickListener(v -> {
+            selectedPersonality = "Cheerful";
+            btnNext.setEnabled(true);
+        });
+
+        btnPlayful.setOnClickListener(v -> {
+            selectedPersonality = "Lazy";
+            btnNext.setEnabled(true);
+        });
+
+        btnCleanFreak.setOnClickListener(v -> {
+            selectedPersonality = "Playful";
+            btnNext.setEnabled(true);
+        });
+
+        btnCheerful.setOnClickListener(v -> {
+            selectedPersonality = "Clean Freak";
+            btnNext.setEnabled(true);
+        });
     }
 
-    private void goToNextStep() {
-        // Pass username to the Choose Food activity
-        Intent intent = new Intent(this, ChooseFoodActivity.class);
-        intent.putExtra("USERNAME", username);
-        startActivity(intent);
-        finish();
-    }
 }
+
