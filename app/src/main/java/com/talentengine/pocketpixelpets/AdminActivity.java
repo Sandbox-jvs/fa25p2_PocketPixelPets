93aae60229e79e40f034c2612266d1df3742da0c
Cristian <cristianprz123@gmail.com>
2025-12-17 14:09:41 -0800

    Added the logout menu

--- app/src/main/java/com/talentengine/pocketpixelpets/AdminActivity.java
+++ app/src/main/java/com/talentengine/pocketpixelpets/AdminActivity.java
@@ -1,24 +1,103 @@
 package com.talentengine.pocketpixelpets;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.talentengine.pocketpixelpets.database.entities.Action;
 
 public class AdminActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // LOGOUT MENU IMPLEMENTATION - Force update the menu
+        invalidateOptionsMenu();
+
     }
+
+
+
+    // vvv vvv vvv LOGOUT MENU IMPLEMENTATION vvv vvv vvv
+    // When copying the menu implementation, you must also include the following line in OnCreate:
+    // invalidateOptionsMenu(); // Force update the menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logout_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem item = menu.findItem(R.id.logoutMenuItem);
+        item.setVisible(true);
+
+        // Retrieve the username from the previous activity (login or signup)
+        Intent loginIntent = getIntent();
+        String username = loginIntent.getStringExtra("USERNAME");
+
+        // Use the collected username and set the Menu bar title to it
+        item.setTitle(username);
+
+        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(@NonNull MenuItem item) {
+                showLogoutDialog();
+                return false;
+            }
+        });
+        return true;
+    }
+
+    /**
+     * Asks the user if they really want to log out. If so, they will be removed to login screen
+     */
+    private void showLogoutDialog() {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(AdminActivity.this);
+        final AlertDialog alertDialog = alertBuilder.create();
+
+        /*
+         * Display a menu as:
+         * | - - - - - - - - - - - - - - - |
+         * | Do you really want to logout? |
+         * |        Logout | Cancel        |
+         * | - - - - - - - - - - - - - - - |
+         */
+        alertBuilder.setTitle("Do you really want to logout?");
+        alertBuilder.setPositiveButton("Logout", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                logout();
+            }
+        });
+
+        alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                alertDialog.dismiss();
+            }
+        });
+
+        alertBuilder.create().show();
+    }
+
+    private void logout() {
+        Intent intent = new Intent(AdminActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+    // ^^^ ^^^ ^^^ LOGOUT MENU IMPLEMENTATION ^^^ ^^^ ^^^
 }
