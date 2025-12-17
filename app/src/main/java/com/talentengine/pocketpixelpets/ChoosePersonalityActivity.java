9ce8c4d84d2f50f1f7b364f04d34d52668949610
Jessica Sandoval <1991jessica.sandoval@gmail.com>
2025-12-16 18:22:57 -0800

    Cosmetic changes; added selection highlight to Personality Activity

--- app/src/main/java/com/talentengine/pocketpixelpets/ChoosePersonalityActivity.java
+++ app/src/main/java/com/talentengine/pocketpixelpets/ChoosePersonalityActivity.java
@@ -3,9 +3,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.talentengine.pocketpixelpets.database.AppDatabase;
