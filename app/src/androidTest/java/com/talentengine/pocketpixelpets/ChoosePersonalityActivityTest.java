b9daf0b35f57ce3911813b89cbbcd69b3d327d3c
Jessica Sandoval <1991jessica.sandoval@gmail.com>
2025-12-18 18:20:59 -0800

    Test methods for next button created. Navigates before selection passes, but navigate after selection fails.

--- app/src/androidTest/java/com/talentengine/pocketpixelpets/ChoosePersonalityActivityTest.java
+++ app/src/androidTest/java/com/talentengine/pocketpixelpets/ChoosePersonalityActivityTest.java
@@ -2,6 +2,9 @@ package com.talentengine.pocketpixelpets;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -31,6 +34,7 @@ public class ChoosePersonalityActivityTest {
     View lazyCard;
     View playfulCard;
     View cleanFreakCard;
+    View nextButtonPersonality;
 
     /**
      * Creates a rule to launch the activity automatically for every test method
@@ -97,13 +101,114 @@ public class ChoosePersonalityActivityTest {
             assertEquals(IS_DIMMED_ALPHA, playfulAlpha, 0.1f);
             assertEquals(IS_DIMMED_ALPHA, cleanFreakAlpha, 0.1f);
 
-            Log.d("Test", "Cheerful card selected.\n" +
-                    "Cheerful= " + cheerfulAlpha +
-                    "Lazy= " + lazyAlpha +
-                    "Playful" + playfulAlpha +
-                    "Clean Freak= " + cleanFreakAlpha
-            );
         });
     }
 
+    @Test
+    public void lazyCardHighlightsWhenSelected() {
+        onView(withId(R.id.lazyButton)).perform(click());
+
+        //Need to reference the alpha for comparative test
+        rule.getScenario().onActivity(activity -> {
+
+            //Reference material cards
+            lazyCard = activity.findViewById(R.id.lazyButton);
+            cheerfulCard = activity.findViewById(R.id.cheerfulButton);
+            playfulCard = activity.findViewById(R.id.playfulButton);
+            cleanFreakCard = activity.findViewById(R.id.cleanFreakButton);
+
+            //Obtain the current alpha
+            lazyAlpha = lazyCard.getAlpha();
+            cheerfulAlpha = cheerfulCard.getAlpha();
+            playfulAlpha = playfulCard.getAlpha();
+            cleanFreakAlpha = cleanFreakCard.getAlpha();
+
+            //Compare the values to ensure only the cheerful card is highlighted
+            assertEquals(IS_FULL_ALPHA, lazyAlpha, 1.0f);
+            assertEquals(IS_DIMMED_ALPHA, cheerfulAlpha, 0.1f);
+            assertEquals(IS_DIMMED_ALPHA, playfulAlpha, 0.1f);
+            assertEquals(IS_DIMMED_ALPHA, cleanFreakAlpha, 0.1f);
+
+        });
+    }
+
+    @Test
+    public void playfulCardHighlightsWhenSelected() {
+        onView(withId(R.id.playfulButton)).perform(click());
+
+        //Need to reference the alpha for comparative test
+        rule.getScenario().onActivity(activity -> {
+
+            //Reference material cards
+            playfulCard = activity.findViewById(R.id.playfulButton);
+            cheerfulCard = activity.findViewById(R.id.cheerfulButton);
+            lazyCard = activity.findViewById(R.id.lazyButton);
+            cleanFreakCard = activity.findViewById(R.id.cleanFreakButton);
+
+            //Obtain the current alpha
+            playfulAlpha = playfulCard.getAlpha();
+            cheerfulAlpha = cheerfulCard.getAlpha();
+            lazyAlpha = lazyCard.getAlpha();
+            cleanFreakAlpha = cleanFreakCard.getAlpha();
+
+            //Compare the values to ensure only the cheerful card is highlighted
+            assertEquals(IS_FULL_ALPHA, playfulAlpha, 1.0f);
+            assertEquals(IS_DIMMED_ALPHA, cheerfulAlpha, 0.1f);
+            assertEquals(IS_DIMMED_ALPHA, lazyAlpha, 0.1f);
+            assertEquals(IS_DIMMED_ALPHA, cleanFreakAlpha, 0.1f);
+
+        });
+    }
+    @Test
+    public void cleanFreakCardHighlightsWhenSelected() {
+        onView(withId(R.id.cleanFreakButton)).perform(click());
+
+        //Need to reference the alpha for comparative test
+        rule.getScenario().onActivity(activity -> {
+
+            //Reference material cards
+            cleanFreakCard = activity.findViewById(R.id.cleanFreakButton);
+            cheerfulCard = activity.findViewById(R.id.cheerfulButton);
+            lazyCard = activity.findViewById(R.id.lazyButton);
+            playfulCard = activity.findViewById(R.id.playfulButton);
+
+            //Obtain the current alpha
+            cleanFreakAlpha = cleanFreakCard.getAlpha();
+            cheerfulAlpha = cheerfulCard.getAlpha();
+            lazyAlpha = lazyCard.getAlpha();
+            playfulAlpha = playfulCard.getAlpha();
+
+            //Compare the values to ensure only the cheerful card is highlighted
+            assertEquals(IS_FULL_ALPHA, cleanFreakAlpha, 1.0f);
+            assertEquals(IS_DIMMED_ALPHA, cheerfulAlpha, 0.1f);
+            assertEquals(IS_DIMMED_ALPHA, lazyAlpha, 0.1f);
+            assertEquals(IS_DIMMED_ALPHA, playfulAlpha, 0.1f);
+
+        });
+    }
+
+    /**
+     * This test checks to see if the activity changed when next button is clicked before making
+     * a selection. Should pass only if the display is on the same activity
+     */
+    @Test
+    public void nextButtonWillNotNavigateBeforeSelection() {
+        // Activity will not change if next button is clicked before selection
+        onView(withId(R.id.nextButtonPersonality)).perform(click());
+
+        //Check to see if the view is still on the choose personality Activity
+        onView(withId(R.id.choosePersonalityTextView)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void nextButtonNavigatesAfterSelection() {
+        //Click on a card
+        onView(withId(R.id.playfulButton)).perform(click());
+
+        //click on next button
+        onView(withId(R.id.nextButtonPersonality)).check(matches(isDisplayed())).perform(click());
+
+        //Check if activity was changed
+        onView(withId(R.id.chooseFoodSubtitleTextView)).check(matches(isDisplayed()));
+    }
 }
